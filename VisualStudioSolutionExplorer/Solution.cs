30a8640ff38a3632e3be9903314584ba10554d8e
dlebansais <dlebansais@gmail.com>
2021-04-23 23:21:33 +0200

    Added support for reading a solution from a stream.

--- VisualStudioSolutionExplorer/Solution.cs
+++ VisualStudioSolutionExplorer/Solution.cs
@@ -35,14 +35,31 @@
             SolutionFileName = solutionFileName;
             Name = Path.GetFileNameWithoutExtension(solutionFileName);
 
+            using StreamReader Reader = new StreamReader(solutionFileName);
+            Initialize(Reader);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Solution"/> class.
+        /// </summary>
+        /// <param name="name">The solution name.</param>
+        /// <param name="reader">A stream with the solution content.</param>
+        public Solution(string name, StreamReader reader)
+        {
+            SolutionFileName = string.Empty;
+            Name = name;
+
+            Initialize(reader);
+        }
+
+        private void Initialize(StreamReader reader)
+        {
             ConstructorInfo Constructor = ReflectionTools.GetFirstTypeConstructor(SolutionParserType);
             var SolutionParser = Constructor.Invoke(null);
 
-            using StreamReader Reader = new StreamReader(solutionFileName);
-            SolutionParserReader.SetValue(SolutionParser, Reader, null);
+            SolutionParserReader.SetValue(SolutionParser, reader, null);
             SolutionParserParseSolution.Invoke(SolutionParser, null);
 
-            ProjectList = new List<Project>();
             Array ProjetctArray = (Array)ReflectionTools.GetPropertyValue(SolutionParserProjects, SolutionParser);
             for (int i = 0; i < ProjetctArray.Length; i++)
             {
@@ -68,7 +85,7 @@
         /// <summary>
         /// Gets the list of projects in the solution.
         /// </summary>
-        public List<Project> ProjectList { get; init; }
+        public List<Project> ProjectList { get; } = new();
         #endregion
     }
 }

36342dc4e357bdb3a2457592ecf08c7782ed3553
David Le Bansais <dlebansais@gmail.com>
2026-06-03 13:24:25 +0200

    Misc fixes.

--- coverage.bat
+++ coverage.bat
@@ -4,26 +4,28 @@ setlocal
 
 call ..\Certification\set_tokens.bat
 
+set BASE_DIR=%CD%
 set PROJECTNAME=VisualStudioSolutionExplorer
 set TOKEN=%VISUALSTUDIOSOLUTIONEXPLORER_CODECOV_TOKEN%
 set TESTPROJECTNAME=%PROJECTNAME%.Test
 set PLATFORM=x64
 set CONFIGURATION=Debug
-set FRAMEWORK=net8.0
+set FRAMEWORK=net10.0
 set RESULTFILENAME=Coverage-%PROJECTNAME%.xml
-set RESULTFILEPATH=".\Test\%TESTPROJECTNAME%\bin\%PLATFORM%\%CONFIGURATION%\%FRAMEWORK%\%RESULTFILENAME%"
+set RESULTFILEPATH=".\bin\%PLATFORM%\%CONFIGURATION%\%FRAMEWORK%\%RESULTFILENAME%"
+set MODULEFILEPATH=".\bin\%PLATFORM%\%CONFIGURATION%\%FRAMEWORK%\%TESTPROJECTNAME%.dll"
 
 set OPENCOVER_VERSION=4.7.1221
 set OPENCOVER=OpenCover.%OPENCOVER_VERSION%
-set OPENCOVER_EXE=".\packages\%OPENCOVER%\tools\OpenCover.Console.exe"
+set OPENCOVER_EXE=packages\%OPENCOVER%\tools\OpenCover.Console.exe
 
 set CODECOV_UPLOADER_VERSION=0.7.2
 set CODECOV_UPLOADER=CodecovUploader.%CODECOV_UPLOADER_VERSION%
-set CODECOV_UPLOADER_EXE=".\packages\%CODECOV_UPLOADER%\tools\codecov.exe"
+set CODECOV_UPLOADER_EXE=packages\%CODECOV_UPLOADER%\tools\codecov.exe
 
 set REPORTGENERATOR_VERSION=5.2.0
 set REPORTGENERATOR=ReportGenerator.%REPORTGENERATOR_VERSION%
-set REPORTGENERATOR_EXE=".\packages\%REPORTGENERATOR%\tools\net8.0\ReportGenerator.exe"
+set REPORTGENERATOR_EXE=packages\%REPORTGENERATOR%\tools\net8.0\ReportGenerator.exe
 
 nuget install OpenCover -Version %OPENCOVER_VERSION% -OutputDirectory packages
 nuget install CodecovUploader -Version %CODECOV_UPLOADER_VERSION% -OutputDirectory packages
@@ -36,17 +38,18 @@ if not exist %REPORTGENERATOR_EXE% goto error_console4
 
 if exist ".\Test\%TESTPROJECTNAME%\publish" rd /S /Q ".\Test\%TESTPROJECTNAME%\publish"
 
-dotnet build ./Test/%TESTPROJECTNAME% /p:Platform=%PLATFORM% -c %CONFIGURATION% -f %FRAMEWORK%
-
-if exist .\Test\%TESTPROJECTNAME%\*.log del .\Test\%TESTPROJECTNAME%\*.log
-if exist %RESULTFILEPATH% del %RESULTFILEPATH%
+rem dotnet build ./Test/%TESTPROJECTNAME% /p:Platform=%PLATFORM% -c %CONFIGURATION% -f %FRAMEWORK%
 
 rem Execute tests within OpenCover.
-%OPENCOVER_EXE% -register:user -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test -c %CONFIGURATION% -f %FRAMEWORK% --no-build -l console;verbosity=detailed" -output:%RESULTFILEPATH% -mergeoutput
+pushd Test\%TESTPROJECTNAME%
 
-if not exist %RESULTFILEPATH% goto end
-%CODECOV_UPLOADER_EXE% -f %RESULTFILEPATH% -t %TOKEN%
-%REPORTGENERATOR_EXE% -reports:%RESULTFILEPATH% -targetdir:.\CoverageReports "-assemblyfilters:+%PROJECTNAME%;+%TESTPROJECTNAME%" "-filefilters:-*.g.cs;-*Microsoft.NET.Test.Sdk.Program.cs"
+if exist %RESULTFILEPATH% del %RESULTFILEPATH%
+"%BASE_DIR%\%OPENCOVER_EXE%" -register:user -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test --output:detailed --test-modules %MODULEFILEPATH%" -output:%RESULTFILEPATH% -mergeoutput
+if not exist %RESULTFILEPATH% goto error_no_test_result
+
+"%BASE_DIR%\%CODECOV_UPLOADER_EXE%" -f %RESULTFILEPATH% -t %TOKEN%
+"%BASE_DIR%\%REPORTGENERATOR_EXE%" -reports:%RESULTFILEPATH% -targetdir:%BASE_DIR%\CoverageReports "-assemblyfilters:+%PROJECTNAME%;+%TESTPROJECTNAME%" "-filefilters:-*.g.cs;-*Microsoft.NET.Test.Sdk.Program.cs"
+popd
 goto end
 
 :error_console1
@@ -67,6 +70,11 @@ goto end
 
 :error_not_built
 echo ERROR: %TESTPROJECTNAME%.dll not built.
+goto end
+
+:error_no_test_result
+echo ERROR: No test result
+popd
 goto end
 
 :end
